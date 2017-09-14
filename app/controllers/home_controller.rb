b41eac43d2f1935c41cc97e6501acbdb5a077a8a
websitekay <websitekay@gmail.com>
2017-09-13 23:53:14 -0700

    run React on rails view by javascript_pack_tag

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class HomeController < ActionController::Base
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/players", to: 'players#index'
+  root 'home#index'
 end
